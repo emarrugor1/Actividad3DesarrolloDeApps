cc5e5d30c2ddfc08e18d8ccaa48d6c31c1072f44
emarrugor1 <emarrugor1@unicartagena.edu.co>
2023-11-07 21:00:02 -0500

    se crea backend y frontend

---
+++ app/src/main/java/com/example/cipasoldschool/actividad1/actividadunidad3desarrollodeapps/util/MessageVerification.java
@@ -0,0 +1,2 @@
+package com.example.cipasoldschool.actividad1.actividadunidad3desarrollodeapps.util;public class MessageVerification {
+}
--- app/src/main/java/com/example/cipasoldschool/actividad1/actividadunidad3desarrollodeapps/viewmodel/UserViewModel.java
+++
@@ -1,4 +0,0 @@
-package com.example.cipasoldschool.actividad1.actividadunidad3desarrollodeapps.viewmodel;
-
-public class UserViewModel {
-}
