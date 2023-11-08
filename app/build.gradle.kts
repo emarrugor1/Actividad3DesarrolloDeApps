87cd8dc02a7955709cf303fc652dcd8998dec0e0
emarrugor1 <emarrugor1@unicartagena.edu.co>
2023-11-07 21:00:24 -0500

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.cipasoldschool.actividad1.actividadunidad3desarrollodeapps"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.cipasoldschool.actividad1.actividadunidad3desarrollodeapps"
--- app/src/main/java/com/example/cipasoldschool/actividad1/actividadunidad3desarrollodeapps/util/MessageVerification.java
+++ app/src/main/java/com/example/cipasoldschool/actividad1/actividadunidad3desarrollodeapps/util/MessageVerification.java
@@ -1,2 +1,25 @@
-package com.example.cipasoldschool.actividad1.actividadunidad3desarrollodeapps.util;public class MessageVerification {
+package com.example.cipasoldschool.actividad1.actividadunidad3desarrollodeapps.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageVerification {
+    private static List<String> validarEntrada(String userEmail, String password){
+        List<String> mensajes = new ArrayList<>();
+        if(userEmail.isEmpty()){
+            mensajes.add("* Campo Email Vacio");
+        }
+        if(password.isEmpty()){
+            mensajes.add("* Campo Password Vacio");
+        }
+        return mensajes;
+    }
+    public  static String mostrarMensajeError(String userEmail,String password){
+        List<String> mensajes = validarEntrada(userEmail,password);
+        String errores="";
+        for(String mensaje:mensajes){
+            errores +=mensaje+"\n";
+        }
+        return errores.trim();
+    }
 }
